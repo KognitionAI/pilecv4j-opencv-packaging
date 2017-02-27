fb77e7bc1322a515ddfaab0fcf96b88f2c49ddc2
Jim Carroll <jim@dontcallme.com>
2017-02-27 12:20:09 -0500

    minor cleanup of build(package) script.

--- package.sh
+++ package.sh
@@ -19,14 +19,19 @@ if [ "$MVN" = "" ]; then
     MVN=mvn
 fi
 
-OPENCV_VERSION=`grep OpenCV_VERSION "$OPENCV_INSTALL/share/OpenCV/OpenCVConfig-version.cmake" | head -1 | sed -e 's/^.*set(.*OpenCV_VERSION *//g' | sed -e 's/).*$//g'`
+if [ "$GIT" = "" ]; then
+    GIT=git
+fi
 
+OPENCV_VERSION=`grep OpenCV_VERSION "$OPENCV_INSTALL/share/OpenCV/OpenCVConfig-version.cmake" | head -1 | sed -e 's/^.*set(.*OpenCV_VERSION *//g' | sed -e 's/).*$//g'`
 OPENCV_SHORT_VERSION=`echo "$OPENCV_VERSION" | sed -e 's/\.//g'`
 
+echo "==========================================="
 echo "Building with:"
 echo "OPENCV_INSTALL=$OPENCV_INSTALL"
 echo "OPENCV_VERSION=$OPENCV_VERSION"
 echo "OPENCV_SHORT_VERSION=$OPENCV_SHORT_VERSION"
+echo "==========================================="
 
 echo ""
 echo "Setting version in the project."
@@ -47,8 +52,5 @@ if [ "$?" -ne 0 ]; then
     exit 1
 fi
 
-git reset --hard HEAD
-git clean -dxf
-
-
-
+$GIT reset --hard HEAD
+$GIT clean -dxf
