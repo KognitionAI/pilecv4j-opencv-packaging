64a19596d6d5df549804e89d207c2eb0aba257b8
Jim Carroll <jim@dontcallme.com>
2017-02-27 12:07:48 -0500

    Getting auto-versioning working.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
+
+
 Install the opencv packaging using maven by pointing to the opencv install by setting OPENCV_INSTALL.
 
 The version of the install should match the pom.xml version. At the time of this writing that 3.2.0.
 
 On windows, from git-bash you can invoke something like:
 
-OPENCV_INSTALL=`cygpath -w /c/Users/user/projects/opencv-3.2.0` mvn clean install
+```OPENCV_INSTALL=`cygpath -w /c/Users/user/projects/opencv-3.2.0` OPENCV_VERSION=3.2.0 OPENCV_SHORT_VERSION=320 mvn clean install```
 
 on Linux it's more straightforward
 
-OPENCV_INSTALL="[absolute path to opencv-3.2.0]" mvn clean install
+OPENCV_INSTALL="[absolute path to opencv-3.2.0]" OPENCV_VERSION=3.2.0 OPENCV_SHORT_VERSION=320 mvn clean install
 
+When building opencv on Linux, you need to set JAVA_HOME and also have 'ant' installed. These are not done by default when following the instructions. The following worked for me after installing ant:
 
+```JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/jim/utils/opencv-3.2.0 ..```
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$OPENCV_INSTALL" = "" ]; then
+    echo "OPENCV_INSTALL must be set to point to the path where OpenCV is installed."
+    exit 1
+fi
+
+if [ ! -d "$OPENCV_INSTALL" ]; then
+    echo "\"$OPENCV_INSTALL\" doesn't exist or isn't a directory."
+    exit 1
+fi
+
+if [ ! -f "$OPENCV_INSTALL/share/OpenCV/OpenCVConfig-version.cmake" ]; then
+    echo "Can't find the build file \"$OPENCV_INSTALL/share/OpenCV/OpenCVConfig-version.cmake\" in order to extract the version."
+    exit 1
+fi
+
+OPENCV_VERSION=`grep OpenCV_VERSION "$OPENCV_INSTALL/share/OpenCV/OpenCVConfig-version.cmake" | head -1 | sed -e 's/^.*set(.*OpenCV_VERSION *//g' | sed -e 's/).*$//g'`
+
+OPENCV_SHORT_VERSION=`echo "$OPENCV_VERSION" | sed -e 's/\.//g'`
+
+echo "Building with:"
+echo "OPENCV_INSTALL=$OPENCV_INSTALL"
+echo "OPENCV_VERSION=$OPENCV_VERSION"
+echo "OPENCV_SHORT_VERSION=$OPENCV_SHORT_VERSION"
+
+echo ""
+echo "Setting version in the project."
+
+/home/jim/utils/maven/bin/mvn versions:set -DnewVersion=$OPENCV_VERSION
+if [ "$?" -ne 0 ]; then
+    echo "Failed to set version. Please manually reset the project using \"git reset --hard HEAD\""
+    exit 1
+fi
+
+
