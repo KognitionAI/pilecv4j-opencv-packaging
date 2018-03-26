a795e787f50dc2cf62ca9003c9227173c92ae399
Jim Carroll <jim@dontcallme.com>
2018-03-25 22:02:02 -0400

    Moved the CvRasterNative class to JNA.

--- package-native/src/main/cpp/sources/CvRasterNative.cpp
+++ package-native/src/main/cpp/sources/CvRasterNative.cpp
@@ -1,70 +1,40 @@
-#include "com_jiminger_image_CvRasterNative.h"
 
 #include <cstdint>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/highgui.hpp>
 
-static void throwIllegalStateException( JNIEnv *env, const char* message ) {
-  static const char* iseClassName = "java/lang/IllegalStateException";
-
-  jclass exClass = env->FindClass(iseClassName);
-  if (exClass == NULL)
-    return; // nothing I can do, really.
-
-  env->ThrowNew(exClass, message );
+extern "C" {
+  void* CvRaster_getData(uint64_t native);
+  uint64_t CvRaster_copy(uint64_t native);
+  uint64_t CvRaster_makeMatFromRawDataReference(uint32_t rows, uint32_t cols, uint32_t type, uint64_t dataLong);
 }
 
-JNIEXPORT jobject JNICALL Java_com_jiminger_image_CvRasterNative__1getData(JNIEnv * env, jclass, jlong native) {
+void* CvRaster_getData(uint64_t native) {
   cv::Mat* mat = (cv::Mat*) native;
 
-  if (!mat->isContinuous()) {
-    throwIllegalStateException(env, "Cannot create a CvRaster from a Mat without a continuous buffer.");
+  if (!mat->isContinuous())
     return NULL;
-  }
 
-  jlong capacity = ((jlong)mat->total() * (jlong)mat->elemSize());
-
-  return env->NewDirectByteBuffer(mat->ptr(0), capacity);
+  return mat->ptr(0);
 }
 
-//JNIEXPORT void JNICALL Java_com_jiminger_image_CvRasterNative_showImage(JNIEnv *, jclass, jlong native) {
-//  cv::Mat* mat = (cv::Mat*) native;
-//  cv::imshow("Window", (*mat));
-//}
-
-JNIEXPORT jlong JNICALL Java_com_jiminger_image_CvRasterNative__1getDataAddress(JNIEnv * env, jclass, jlong native) {
+uint64_t CvRaster_copy(uint64_t native) {
   cv::Mat* mat = (cv::Mat*) native;
 
-  if (!mat->isContinuous()) {
-    throwIllegalStateException(env, "Cannot extract Mat data address form a Mat without a continuous buffer.");
-    return -1;
-  }
-
-  jlong ret = (jlong) mat->ptr(0);
-  return ret;
-}
-
-JNIEXPORT jlong JNICALL Java_com_jiminger_image_CvRasterNative__1copy(JNIEnv * env, jclass, jlong native) {
-  cv::Mat* mat = (cv::Mat*) native;
-
-  if (!mat->isContinuous()) {
-    throwIllegalStateException(env, "Cannot copy a Mat is that Mat doesn't have a continuous buffer.");
-    return -1;
-  }
+  if (!mat->isContinuous())
+    return 0L;
 
   cv::Mat* newMat = new cv::Mat((*mat));
-
-  jlong ret = (jlong) newMat;
-  return ret;
+  return (uint64_t) newMat;
 }
 
-JNIEXPORT jlong JNICALL Java_com_jiminger_image_CvRasterNative__1makeMatFromRawDataReference(JNIEnv * env, jclass, jint rows, jint cols, jint type, jlong dataLong) {
+uint64_t CvRaster_makeMatFromRawDataReference(uint32_t rows, uint32_t cols, uint32_t type, uint64_t dataLong) {
   void* data = (void*) dataLong;
   cv::Mat* newMat = new cv::Mat(rows, cols, type, data);
 
-  jlong ret = (jlong) newMat;
-  return ret;
+  return (uint64_t) newMat;
 }
+
 
 
 
